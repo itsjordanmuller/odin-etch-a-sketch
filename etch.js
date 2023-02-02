d2c6bf1612a8b228dd08abfdb61b72034d034b09
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-01 20:13:44 -0800

    Starting from scratch on etch.js, with a few days distance from the problem

--- etch.js
+++ etch.js
@@ -1,12 +1,1 @@
-const container = document.querySelector(".grid-container");
-
-function createGrid(size) {
-  container.style.setProperty("--grid-rows", size);
-  container.style.setProperty("--grid-cols", size);
-  for (let i = 0; i < (size * size); i++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-
-createGrid(16);
+// Etch-a-Sketch JavaScript
