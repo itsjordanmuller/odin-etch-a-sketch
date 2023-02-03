06724d8dabaa69a615bbe04e4fb9de0bd8b64903
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-03 14:14:23 -0800

    Add loop for columns, set bg, addEventListeners, append cells and rows

--- etch.js
+++ etch.js
@@ -17,5 +17,17 @@ function createGrid() {
   for (let r = 0; r < rows; r++) {
     const row = document.createElement("div");
     row.classList.add("row");
+
+    for (let c = 0; c < columns; c++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.style.backgroundColor = colorPicker.value;
+      cell.addEventListener("click", handleCellClick);
+      cell.addEventListener("mouseover", handleCellHover);
+
+      row.appendChild(cell);
+    }
+
+    gridContainer.appendChild(row);
   }
 }
