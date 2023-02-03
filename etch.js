f93fe65803a719c87d6a6c55228dc12a259a7c37
itsjordanmuller <itsjordanmuller@gmail.com>
2023-02-02 18:05:50 -0800

    Modified etch.js to add selectors for button, grid & color input

--- etch.js
+++ etch.js
@@ -1,1 +1,4 @@
-// Etch-a-Sketch JavaScript
+// Return Elements Using Selectors for Button, Grid, & Color Input
+const btnCreateGrid = document.querySelector(".btn-create-grid");
+const gridContainer = document.querySelector(".grid");
+const colorPicker = document.querySelector("input[type=color]");
